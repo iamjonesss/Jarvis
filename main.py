af9849c3c48f2b039b2734f944cc6fcdec785497
unknown <joao.vitorsouzasilva191@gmail.com>
2023-12-24 18:44:20 -0300

    docs: Criação do arquivo requirements.txt

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import speech_recognition as sr
 import pyttsx3 as tsx
 
 try:
+    
     with sr.Microphone() as source:
-        print('Ouvindo...')
         voz = audio.listen(source)
         comando = audio.recognize_google(voz, language='pt-BR')
         comando = comando.lower()
-            
+        
         
 except Exception as e:
-    print(f'Microfone não reconhecido, erro: {e}')
+    print(f'Erro na aplicação: {e}')
